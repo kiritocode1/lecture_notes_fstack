9fdc0700a6f1fb690eb248c8f0d9626072dabd39
kiritocode1 <kathawalearyan9@gmail.com>
2021-11-24 02:12:59 +0530

    daybreak

--- dom-project-with-bs/script.js
+++ dom-project-with-bs/script.js
@@ -1,3 +1,10 @@
-let func = () => {
-    alert("hello there")
+const saveChanges = () => {
+    const taskData = {
+        id: `${Date.now()}`, //return  the unique functions .
+        imageurl: document.getElementById("imageURL").value,
+        tasktitle: document.getElementById("taskTitle").value,
+        tasktype: document.getElementById("taskType").value,
+        taskdescription: document.getElementById("taskDescription").value
+    };
+    console.log(taskData);
 }
