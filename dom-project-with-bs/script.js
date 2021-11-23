d712014e9cc247602434b60fdeb033aa08e5dd3a
kiritocode1 <kathawalearyan9@gmail.com>
2021-11-24 02:44:18 +0530

    daybreak

--- dom-project-with-bs/script.js
+++ dom-project-with-bs/script.js
@@ -1,3 +1,6 @@
+const taskContainer = document.getElementById("task_container");
+console.log(taskContainer[0]);
+
 const saveChanges = () => {
     const taskData = {
         id: `${Date.now()}`, //return  the unique functions .
@@ -7,4 +10,21 @@ const saveChanges = () => {
         taskdescription: document.getElementById("taskDescription").value
     };
     console.log(taskData);
+    //!grabbing the data from users for push .
+const new_card = `<div  id = ${taskData.id} class="col-md-6 col-lg-4">
+                        <div class="card">
+                            <div class="card-header">
+                                <button type="button" class="btn btn-outline-success"><i class="fas fa-pen"></i></button>
+                                <button type="button" class="btn btn-outline-danger"><i class="fas fa-trash"></i></button>
+                            </div>
+                                <img src= ${taskData.imageurl} alt="img" id="main-img">
+                            <div class="card-body">
+                                <h5 class="card-title text-primary"><strong>${taskData.tasktitle}</strong></h5>
+                                <p class="card-text">${taskData.taskdescription}</p>
+                                <a href="#" class="btn btn-primary">click-click</a>
+                            </div>
+                        </div>
+                    </div>
+`;
+    taskContainer.insertAdjacentHTML
 }
